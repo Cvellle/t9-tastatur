f2392b1d7459df00a96dab47dc2893aa0141eded
Cvele <cuekisa@yahoo.com>
2018-10-29 01:30:40 +0100

    A
    A

--- src2/store/actionNames.js
+++
@@ -1,5 +0,0 @@
-export const SET_LOADING = 'SET_LOADING';
-export const SET_NUMBERS = 'SET_NUMBERS';
-export const SET_OUTPUT = 'SET_OUTPUT';
-export const SET_FILTERED = 'SET_FILTERED';
-export const FILTER_NUMBERS = 'FILTER_NUMBERS';
